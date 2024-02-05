8d3e4aa5d36bcc9f5a03115d7786ae4d10d0d0d9
Keenan Villani-Holland <villanih@adobe.com>
2024-02-05 15:22:33 -0800

    fix enter in nvim

--- .config/nvim/lua/plugins/nvim-cmp-config.lua
+++ .config/nvim/lua/plugins/nvim-cmp-config.lua
@@ -1,5 +1,6 @@
 return {
 	"hrsh7th/nvim-cmp",
+	dependencies = { "hrsh7th/cmp-nvim-lsp", "hrsh7th/cmp-buffer", "saadparwaiz1/cmp_luasnip", "L3MON4D3/LuaSnip" },
 	config = function()
 		local cmp = require("cmp")
 		local luasnip = require("luasnip")
