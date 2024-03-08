c6cd9fd693cd7e03dcaca163a7a1048417f73e13
keenan <keenan.vh@icloud.com>
2024-03-08 15:37:50 -0800

    add brew updgrade to update script

--- dotfiles/update.sh
+++ dotfiles/update.sh
@@ -3,6 +3,7 @@
 cd ~/.config
 brew cleanup
 brew update
+brew upgrade
 brew install font-fira-code-nerd-font nvim node python3 yarn npm fx ripgrep sketchybar jq fzf fff gnu-sed tree-sitter eza btop
 source "$HOME/.zshrc"
 brew services restart sketchybar
