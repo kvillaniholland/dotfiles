25f89e3f300a8a12bc4d8c35fefdd2eea9a6476d
keenan <keenan.vh@icloud.com>
2024-03-08 15:47:24 -0800

    add ffmpeg

--- dotfiles/update.sh
+++ dotfiles/update.sh
@@ -4,6 +4,6 @@ cd ~/.config
 brew cleanup
 brew update
 brew upgrade
-brew install font-fira-code-nerd-font nvim node python3 yarn npm fx ripgrep sketchybar jq fzf fff gnu-sed tree-sitter eza btop
+brew install ffmpeg font-fira-code-nerd-font nvim node python3 yarn npm fx ripgrep sketchybar jq fzf fff gnu-sed tree-sitter eza btop
 source "$HOME/.zshrc"
 brew services restart sketchybar
